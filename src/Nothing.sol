0af591332424d0275cad2201d71eb1d84b22746f
HackFisher <hackfisher@gmail.com>
2017-11-30 17:02:38 +0800

    addin Nothing contract for testing transfer on mainnet

---
+++ src/Nothing.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.13;
+
+import "./auth.sol";
+import "./erc20.sol";
+
+contract Nothing is DSAuth {
+    // do not have receiveToken API
+    
+    function claimTokens(address _token) public auth {
+        if (_token == 0x0) {
+            owner.transfer(this.balance);
+            return;
+        }
+
+        ERC20 token = ERC20(_token);
+        uint256 balance = token.balanceOf(this);
+        token.transfer(owner, balance);
+        ClaimedTokens(_token, owner, balance);
+    }
+    
+    event ClaimedTokens(address indexed _token, address indexed _controller, uint256 _amount);
+}
